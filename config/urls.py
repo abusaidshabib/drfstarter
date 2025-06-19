7169cdb4a2a73da846743ffba8735e880cc9a757
abusaidshabib <abusaidshabib712@gmail.com>
2025-06-19 19:33:32 +0600

    updated

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 from django.conf.urls.static import static
 
 apidoc = [
-    # YOUR PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
     path('api/schema/swagger-ui/',
          SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/',
